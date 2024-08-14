9cffe548712a528eabf23f50d90bec0418fe3143
frappaaa <francescoluppi2@gmail.com>
2024-08-14 23:07:08 +0200

    UPD: creazione sito per traduzione

---
+++ 02 - Traduttore/script.js
@@ -0,0 +1,31 @@
+const langButtons = document.querySelectorAll(".lang-button");
+const textInput = document.querySelector(".text-input");
+const translationText = document.querySelector(".translation-text");
+const translationFlag = document.querySelector(".translation-flag");
+const resetButton = document.querySelector(".reset-button");
+
+function reset() {
+  textInput.value = "";
+  translationText.innerText = "Traduzione";
+  translationFlag.innerText = "";
+}
+
+async function translate(text, lang, flag) {
+  const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=it|${lang}`;
+  const response = await fetch(url);
+  const jsonData = await response.json();
+  const result = jsonData.responseData.translatedText;
+  translationText.innerText = result;
+  translationFlag.innerText = flag;
+}
+
+langButtons.forEach(function (langButton) {
+  langButton.addEventListener("click", function () {
+    const text = textInput.value;
+    const lang = langButton.dataset.lang;
+    const flag = langButton.innerText;
+    translate(text, lang, flag);
+  });
+});
+
+resetButton.addEventListener("click", reset);
