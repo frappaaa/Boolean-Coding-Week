bf5fa5c8ed941d4ff80cc9eaf5d93e1077ea7811
frappaaa <francescoluppi2@gmail.com>
2024-08-18 18:39:53 +0200

    UPD: fine progetto

--- 03 - Meteo/script.js
+++ 03 - Meteo/script.js
@@ -15,11 +15,53 @@ function onError() {
   htmlElement.className = "";
 }
 
-function onSuccess() {
-  weatherLocation.innerText = "";
-  weatherIcon.alt = "Geolocation disattivata";
-  weatherIcon.src = "images/geolocation_disabled.png";
-  suggestion.innerText = "Non hai abilitato la localizzazione";
+async function onSuccess() {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const API_KEY = "a66a460e9767dbb6554509b9ff702774";
+  const units = "metric";
+  const lang = "it";
+
+  const endpoint = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${units}&lang=${lang}`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.log(data);
+
+  const iconCode = data.weather[0].icon;
+  const description = data.weather[0].description;
+
+  weatherLocation.innerText = data.name;
+  weatherIcon.alt = description;
+  weatherIcon.src = `images/${iconCode}.png`;
+  weatherTemperature.innerText = `${Math.floor(data.main.temp)}°`;
+  suggestion.innerText = getSuggestion(iconCode);
 
   htmlElement.className = "";
+}
+
+function getSuggestion(iconCode) {
+  const suggestions = {
+    "01d": "Ricordati la crema solare!",
+    "01n": "Buonanotte!",
+    "02d": "Oggi il sole va e viene...",
+    "02n": "Attenti ai lupi mannari...",
+    "03d": "Luce perfetta per fare foto!",
+    "03n": "Dormi sereno :)",
+    "04d": "Che cielo grigio :(",
+    "04n": "Non si vede nemmeno la luna!",
+    "09d": "Prendi l'ombrello",
+    "09n": "Copriti bene!",
+    "10d": "Prendi l'ombrello",
+    "10n": "Copriti bene!",
+    "11d": "Attento ai fulmini!",
+    "11n": "I lampi accendono la notte!",
+    "13d": "Esci a fare un pupazzo di neve!",
+    "13n": "Notte perfetta per stare sotto il piumone!",
+    "50d": "Accendi i fendinebbia!",
+    "50n": "Guida con prudenza!",
+  };
+
+  return suggestions[iconCode];
 }
